ac014788b8910a7620eac2cbcdba026d0fba98b7
Franco <frank.13.gp@gmail.com>
2023-04-07 17:36:52 -0500

    Act.1.2

--- act1.c
+++ act1.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+    struct Persona
+    {
+        char name[50];
+        char apellidos[50];
+        char codigo[11];
+        int celular;
+        char email[50];
+    };
+
 int main()
 {
+    struct Persona alumno = {"Franco", "P.M", "U123456789",987654321,"Franco@hotmail.com"};
+
     printf("Bienvenidos estudiantes al curso de de Arquitectura de Computadoras \n");
+    //Imprimir informacion
+    printf("mi nombre es: %s\n",alumno.name);
+    printf("mi apellido es: %s\n",alumno.apellidos);
+    printf("mi codigo es: %s\n",alumno.codigo);
+    printf("mi celular es: %d\n",alumno.celular);
+    printf("mi Email es: %s\n",alumno.email);
     return 0;
 }
